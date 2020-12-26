db6a4c7ca72fae190d80b692de3f6005cef9b599
Nepenthes <l>
2020-12-26 15:33:26 +0800

    更新批量ID生成方法.

--- homekit批量ID生成/homekit_ID排序.c
+++ homekit批量ID生成/homekit_ID排序.c
@@ -11,8 +11,11 @@ typedef struct _stt_dataHandle_typeA{
 	int num_B;
 	int num_C;
 	
-	char setup_code[16];
-	char setup_payload[32];
+	char *setup_id;
+	char *setup_code;
+	char *setup_salt;
+	char *setup_verifier;
+	char *setup_payload;
 }_stt_dHle_type_A;
 
 
@@ -35,32 +38,42 @@ int cmpUsrApp_A(const void *a,const void *b)
 	return res;
 }
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
 void func_hkPayloadExtract(void){
 	
 	_stt_dHle_type_A dataSort[10000] = {0};
 
     //打开文件 
-    FILE * r=fopen("C:\\Users\\Administrator\\Desktop\\csvDataHandle.txt","r");
+    FILE * r=fopen("C:\\Users\\Nepenthes\\Desktop\\csvDataHandle.txt","r");
     assert(r!=NULL);
-    FILE * w=fopen("C:\\Users\\Administrator\\Desktop\\hkPayload.txt","w");
+    FILE * w=fopen("C:\\Users\\Nepenthes\\Desktop\\hkPayload.txt","w");
     assert(w!=NULL);
      
     //读写文件 
-    char a[64] = {0},
-    	 b[64] = {0};
+    char a[32] = {0},
+    	 b[32] = {0},
+    	 c[64] = {0},
+    	 d[936] = {0},
+    	 e[64] = {0};
     int num_A = 0,
     	num_B = 0,
     	num_C = 0;
     int i=0;
     int loop = 0;
     
-    while(fscanf(r,"%*[^,],%[^,],%*[^,],%*[^,],%s", a, b)!=EOF)
+    while(fscanf(r,"\n%[^,],%[^,],%[^,],%[^,],%s", a, b, c, d, e)!=EOF)
     {
-    	sscanf(a, "%d-%d-%d", &dataSort[i].num_A, &dataSort[i].num_B, &dataSort[i].num_C);
-		strcpy(dataSort[i].setup_code, a);
-		strcpy(dataSort[i].setup_payload, b);
+    	dataSort[i].setup_id = (char *)calloc(1, sizeof(char) * 32);
+    	dataSort[i].setup_code = (char *)calloc(1, sizeof(char) * 32);
+    	dataSort[i].setup_salt = (char *)calloc(1, sizeof(char) * 64);
+    	dataSort[i].setup_verifier = (char *)calloc(1, sizeof(char) * 936);
+    	dataSort[i].setup_payload = (char *)calloc(1, sizeof(char) * 64);
+    	
+    	sscanf(b, "%d-%d-%d", &dataSort[i].num_A, &dataSort[i].num_B, &dataSort[i].num_C);
+		strcpy(dataSort[i].setup_id, a);
+		strcpy(dataSort[i].setup_code, b);
+		strcpy(dataSort[i].setup_salt, c);
+		strcpy(dataSort[i].setup_verifier, d);
+		strcpy(dataSort[i].setup_payload, e);
 		
 		printf("loop %d handle done,\n", i);
 		
@@ -74,7 +87,17 @@ void func_hkPayloadExtract(void){
     for(loop = 0; loop < i; loop ++){
     	
     	printf("%s\n", dataSort[loop].setup_code);
-    	fprintf(w, "%s,%s\n", dataSort[loop].setup_code, dataSort[loop].setup_payload);
+    	fprintf(w, "%s,%s,%s,%s,%s\n", dataSort[loop].setup_id,
+									   dataSort[loop].setup_code, 
+									   dataSort[loop].setup_salt,
+									   dataSort[loop].setup_verifier,
+									   dataSort[loop].setup_payload);
+    	
+    	free(dataSort[i].setup_id);
+    	free(dataSort[i].setup_code);
+    	free(dataSort[i].setup_salt);
+    	free(dataSort[i].setup_verifier);
+    	free(dataSort[i].setup_payload);
 	}
      
     //关闭文件 
